1b3ff34f9ae6913a5717a7b3a87bbd1ef91e687d
Richie Rizal <richierizal127@gmail.com>
2025-02-03 00:15:23 +0100

    basic layout of login and sign up fragment

--- app/src/main/java/com/kijiri/aurora/ui/authentication/CreateAccountFragment.java
+++ app/src/main/java/com/kijiri/aurora/ui/authentication/CreateAccountFragment.java
@@ -1,9 +1,12 @@
 package com.kijiri.aurora.ui.authentication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,11 +27,30 @@ public class CreateAccountFragment extends Fragment {
         View root = binding.getRoot();
 
         TextView alreadyHaveAccountLink = root.findViewById(R.id.alreadyHaveAccountLink);
+        EditText emailInput = root.findViewById(R.id.crateEmail);
+        EditText passwordInput = root.findViewById(R.id.createPassword);
+        EditText passwordVerificationInput = root.findViewById(R.id.createPasswordVerification);
+        Button signUpButton = root.findViewById(R.id.signUpButton);
 
         alreadyHaveAccountLink.setOnClickListener(view -> {
             NavHostFragment.findNavController(CreateAccountFragment.this)
                     .navigate(R.id.action_createAccountFragment_to_loginFragment);
         });
+
+        signUpButton.setOnClickListener(view -> {
+            String email = emailInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            String passwordVerification = passwordVerificationInput.getText().toString();
+
+            Log.d(
+                    "CreateAccountFragment",
+                "Sign up button clicked with email: " + email + " and password: " + password
+            );
+
+            // TODO: Validate email and password
+            // TODO: Create user with email and password
+        });
+
 
 
         return root;
